711a2e2982158bf99335fc367f4f50cc98c9071b
valeria farfan <valeria073104@gmail.com>
2018-06-05 21:20:41 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,10 +1,11 @@
-def levitation_force
+def using_while
 	levitation_force = 6
 while levitation_force < 10 
 	levitation_force=levitation_force+1 
 	#your code here
 	
 	end
-levitation_force
+using_while
+
 
 
